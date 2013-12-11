e21f4d48da44e6be3f44cd343d4c4579a4b2335a
Yuichi Tateno <hotchpotch@gmail.com>
2013-12-11 21:23:28 +0900

    Update sample code

--- GlitchKitExample/GlitchKitExample/ViewController.m
+++ GlitchKitExample/GlitchKitExample/ViewController.m
@@ -21,16 +21,23 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     UIImageView* imageView = (UIImageView*)[self.view viewWithTag:100];
+    imageView.userInteractionEnabled = YES;
     
     // glitch
-    [imageView glitch];
-    
+    //[imageView glitch];
+
     // glitch by custom block
-    /*
     [imageView glitchWithBlock:^int(int byte, int index, uint length, Byte *bytes) {
-        return (byte == 42) ? 0 : byte;
+        return (byte == 42 && arc4random() % 30 == 1) ? 0 : byte;
     }];
-     */
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UIImageView* imageView = (UIImageView*)[self.view viewWithTag:100];
+    [imageView glitchWithBlock:^int(int byte, int index, uint length, Byte *bytes) {
+        return (byte == 42 && arc4random() % 300 == 1) ? 0 : byte;
+    }];
 }
 
 - (void)didReceiveMemoryWarning
